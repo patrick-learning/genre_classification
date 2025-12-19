3fe99ceabe2803f1e6dbf8dcfa9daf21b2253fb3
Patrick L(earning) <patrick.l@udacity.com>
2025-12-19 14:53:05 +0100

    feat: my solution

--- check_data/test_data.py
+++ check_data/test_data.py
@@ -111,10 +111,10 @@ def test_kolmogorov_smirnov(data, ks_alpha):
 
     for col in columns:
 
-        ts, p_value = scipy.stats.ks_2samp(sample1[col], sample2[col])
+        result = scipy.stats.ks_2samp(sample1[col], sample2[col])
 
         # NOTE: as always, the p-value should be interpreted as the probability of
         # obtaining a test statistic (TS) equal or more extreme that the one we got
         # by chance, when the null hypothesis is true. If this probability is not
         # large enough, this dataset should be looked at carefully, hence we fail
-        assert p_value > alpha_prime
+        assert result.pvalue > alpha_prime
